3d40212c9fdb2fe2a0c19928e8ed63f7726a64b5
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-09-04 16:18:43 +0100

    app module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectorRef} from '@angular/core';
+import {MqttService, MqttConnectionState} from 'ngx-mqtt';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {set, get} from 'lodash';
+
+export type QoS = 0 | 1 | 2;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-
 export class AppComponent {
-  title = 'app';
+  public topic: string;
+  public retain: boolean;
+  public qos: QoS = 0;
+  public filter: string;
+  public message: string;
+
+  public messages: object;
+
+  public get state(): BehaviorSubject<MqttConnectionState> {
+    return this.mqtt.state;
+  }
+
+  public get weather(): object {
+    return get(this.messages, 'weather', {});
+  }
+
+  public get cameras(): Array<string> {
+    return get(this.messages, 'cameras', []);
+  }
+
+  constructor(private mqtt: MqttService, private cdRef: ChangeDetectorRef) {
+    mqtt.onMessage.subscribe(m => {
+      let topic = m.topic.replace('/', '.');
+      topic = topic.replace(/[\.|\/]?([\d]+)/g, '[$1]');
+      this.messages = this.messages || new Object;
+      try {
+        set(this.messages, topic, JSON.parse(m.payload.toString()));
+      }
+      catch (e) {
+        set(this.messages, topic, m.payload.toString());
+      }
+    });
+    this.subscribe('alarm/#');
+    this.subscribe('cameras/#');
+    this.subscribe('weather/currently');
+    this.subscribe('weather/minutely');
+    this.subscribe('weather/hourly/data');
+    this.subscribe('weather/daily/data');
+    this.subscribe('zwave/switch/195');
+  }
+
+  public subscribe(filter: string): void {
+    this.mqtt.observe(filter);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,61 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {KeysPipe, StateToStringPipe, StateToClassPipe} from './pipes';
+import {FormsModule} from '@angular/forms';
+
+import {
+  MqttMessage,
+  MqttModule,
+  MqttService,
+  MqttServiceOptions,
+  OnMessageEvent
+} from 'ngx-mqtt';
+
+import {WeatherComponent} from './weather/weather.component';
+import {WeatherIconComponent} from './weather-icon/weather-icon.component';
+import {CamerasComponent} from './cameras/cameras.component';
+import {AlarmComponent} from './alarm/alarm.component';
+import {AlarmStateToStringPipe} from './alarm/alarm.pipes';
+import { DoorbellComponent } from './doorbell/doorbell.component';
+
+export const MQTT_SERVICE_OPTIONS: MqttServiceOptions = {
+  hostname: 'mqtt.p.cns.me',
+  username: 'magicmirror',
+  password: 'B5F574A8-6CD0-42E5-83E2-EE0549F36D52',
+  protocol: 'wss',
+  path: '/mqtt',
+  port: 443
+};
+
+export function mqttServiceFactory() {
+  return new MqttService(MQTT_SERVICE_OPTIONS);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    MqttModule.forRoot({
+      provide: MqttService,
+      useFactory: mqttServiceFactory
+    })
+  ],
+  declarations: [
+    AppComponent,
+    KeysPipe,
+    StateToStringPipe,
+    StateToClassPipe,
+    WeatherComponent,
+    WeatherIconComponent,
+    CamerasComponent,
+    AlarmComponent,
+    AlarmStateToStringPipe,
+    DoorbellComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
