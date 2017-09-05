f3c84ae82f5ab108e769376684241fca0ec78d69
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-09-05 14:42:50 +0100

    camera tests

--- src/app/cameras/cameras.component.spec.ts
+++ src/app/cameras/cameras.component.spec.ts
@@ -1,25 +1,37 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CamerasComponent } from './cameras.component';
+import {CamerasComponent} from './cameras.component';
 
 describe('CamerasComponent', () => {
   let component: CamerasComponent;
   let fixture: ComponentFixture<CamerasComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CamerasComponent ]
+      declarations: [CamerasComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CamerasComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
+    component.cameras = ['one', 'two'];
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show a feed for each camera', () => {
+    expect(compiled.querySelectorAll('img').length).toBe(2);
+  });
+
+  it('should inject the camera url into the img src', () => {
+    expect(compiled.querySelector('img').src).toContain('one');
+  });
+
 });
