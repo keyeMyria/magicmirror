f3b3a5d862d261247961b24216bcc16afd66071c
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-09-05 14:37:12 +0100

    more alarm tests

--- src/app/alarm/alarm.component.spec.ts
+++ src/app/alarm/alarm.component.spec.ts
@@ -6,6 +6,7 @@ import {AlarmStateToStringPipe} from './alarm.pipes';
 describe('AlarmComponent', () => {
   let component: AlarmComponent;
   let fixture: ComponentFixture<AlarmComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,6 +18,7 @@ describe('AlarmComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AlarmComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
@@ -53,5 +55,85 @@ describe('AlarmComponent', () => {
     it('should filter out untroubled zones', () =>
       expect(component.troubledZones).not.toContain(jasmine.objectContaining({subtype: 'V_CONTACT'}))
     );
+  });
+
+  describe('View', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+      compiled = fixture.debugElement.nativeElement;
+    });
+    describe('armed home', () => {
+      it('should show the correct text when the alarm is armed home', () => {
+        component.alarm = {state: 'Home'};
+        fixture.detectChanges();
+        expect(compiled.querySelector('p').textContent).toContain('Armed Home');
+      });
+    });
+    describe('armed away', () => {
+      it('should show the correct text when the alarm is armed home', () => {
+        component.alarm = {state: 'Away'};
+        fixture.detectChanges();
+        expect(compiled.querySelector('p').textContent).toContain('Armed Away');
+      });
+    });
+
+    describe('disarmed', () => {
+      it('should show the correct text when the alarm is disarmed', () => {
+        component.alarm = {state: 'Disarm'};
+        fixture.detectChanges();
+        expect(compiled.querySelector('p').textContent).toContain('Disarmed');
+      });
+    });
+    // describe('armed away');
+    describe('open doors', () => {
+      beforeEach(() => {
+        component.alarm = {
+          zones: [
+            {subtype: 'CONTACT', troubles: ['something'], location: 'somewhere'},
+            {subtype: 'CONTACT', troubles: ['something'], location: 'somewhere else'},
+          ]
+        };
+        fixture.detectChanges();
+      });
+
+      it('should show a list of troubled zones', () =>
+        expect(compiled.querySelectorAll('li').length).toBe(2));
+
+      it('should show that not ready to arm', () =>
+        expect(compiled.querySelector('p').textContent).toContain('not ready to arm'));
+    });
+
+    describe('ready to arm', () => {
+      it('should show alarm is ready to arm', () => {
+        component.alarm = {status: true};
+        fixture.detectChanges();
+        expect(compiled.querySelector('p').textContent).not.toContain('not ready to arm');
+      });
+    });
+
+    describe('ready to arm', () => {
+      it('should show alarm is ready to arm', () => {
+        component.alarm = {status: false};
+        fixture.detectChanges();
+        expect(compiled.querySelector('p').textContent).toContain('not ready to arm');
+      });
+    });
+
+    describe('closed doors', () => {
+      beforeEach(() => {
+        component.alarm = {
+          zones: [
+            {subtype: 'CONTACT', troubles: null, location: 'somewhere'},
+            {subtype: 'CONTACT', troubles: null, location: 'somewhere else'}
+          ]
+        };
+        fixture.detectChanges();
+      });
+
+      it('should show a list of troubled zones', () =>
+        expect(compiled.querySelectorAll('li').length).toBe(0));
+
+    });
+
   });
 });
